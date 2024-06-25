42658780c1c01103b6529b5f9613442bd8253c8a
JVeloz19 <jveloz@estudiante.uc.cl>
2024-06-25 03:25:31 -0400

    Comentarios para la rata

--- Sets/12 - Ordenamiento/Pregunta 6 📝 Organizando TO-DO.py
+++ Sets/12 - Ordenamiento/Pregunta 6 📝 Organizando TO-DO.py
@@ -10,6 +10,12 @@ class Tarea:
 def criterio1(item):
     urgencia = item.importancia/item.dias_restantes
     return item.estado[-1:],urgencia
+    #Esta linea anterio es pura suerte que la ultima letra de cada estado
+    #Coincide justo al revés, ej: "En progresO", "PendientE" o "TerminadA"
+    #O,E,A por ende si entregamos el reves, seran A,E,O, que casualmente nos sirve
+    # Por esto mismo, al hacer reves, las obtengo ordenadas como corresponde
+    # Igualemento esto es muy especifico del caso, poniendo una serie de if funciona
+    # de forma mas general
 # ༼ つ ◕_◕ ༽つ Completa el codigo ༼ つ ◕_◕ ༽つ
 def mostrar_tareas_ordenadas(lista_tareas):
     ordenada = sorted(lista_tareas,key=criterio1,reverse=True)
